51caaab9cf3d271c15416f403ddd25af80fa557c
Maxim Yakovlev <maxim.e.yakovlev@gmail.com>
2022-02-11 21:54:59 +0300

    added: authRouter.js file

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const authRouter = require("./authRouter");
 require("dotenv").config();
 const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use("/auth", authRouter);
 
 const start = async () => {
   try {
