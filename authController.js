358baa9643fa6c5f138a9ba3337031dafa56696f
Maxim Yakovlev <maxim.e.yakovlev@gmail.com>
2022-02-12 22:13:07 +0300

    installed bcryptjs, authController.js: created function: signup

--- authController.js
+++ authController.js
@@ -1,12 +1,40 @@
+const User = require("./models/User");
+const Role = require("./models/Role");
+const bcrypt = require("bcryptjs");
+
 class authController {
   async signup(req, res) {
     try {
-    } catch (error) {}
+      const { username, password } = req.body;
+      const candidate = await User.findOne({ username });
+      if (candidate) {
+        return res
+          .status(400)
+          .json({ message: "A user with this name already exists." });
+      }
+      const hashPassword = bcrypt.hashSync(password, 7);
+      const userRole = await Role.findOne({ value: "USER" });
+      const user = new User({
+        username,
+        password: hashPassword,
+        roles: [userRole.value],
+      });
+      await user.save();
+      return res.json({
+        message: "The user has been successfully registered.",
+      });
+    } catch (error) {
+      console.log(error);
+      res.status(400).json({ message: "Signup error" });
+    }
   }
 
   async login(req, res) {
     try {
-    } catch (error) {}
+    } catch (error) {
+      console.log(error);
+      res.status(400).json({ message: "Login error" });
+    }
   }
 
   async getUsers(req, res) {
